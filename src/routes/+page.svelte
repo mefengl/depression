85aab4dd3836a2cf87c7b6e46ed443b4eb1da8bc
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-28 16:45:48 +0800

    add some mood

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  const saying = "WHAT A wanderful WORLD";
+
+  $: hearing = "";
+  $: dance = "";
+  $: lucky = "";
+  $: whoami = "";
+
+  setInterval(() => {
+    const words = saying.split(" ");
+    const index = Math.floor(Math.random() * words.length);
+    hearing = words[index];
+
+    dance = Math.random() > 0.5 ? "italic" : "";
+    lucky = Math.random() > 0.5 ? "font-mono" : "font-serif";
+  }, 2000);
+</script>
+
+<div class="w-screen h-screen grid place-items-center impression">
+  <h1 class="text-9xl text-white font-bold select-none {dance} {lucky}">
+    {hearing}
+  </h1>
+</div>
+
+<style>
+  .impression {
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(45deg, red, blue);
+    animation: gradient 6s infinite linear;
+  }
+
+  @keyframes gradient {
+    0% {
+      background: linear-gradient(45deg, red, blue);
+    }
+    25% {
+      background: linear-gradient(45deg, yellow, green);
+    }
+    50% {
+      background: linear-gradient(45deg, orange, purple);
+    }
+    75% {
+      background: linear-gradient(45deg, pink, cyan);
+    }
+    100% {
+      background: linear-gradient(45deg, red, blue);
+    }
+  }
+</style>
